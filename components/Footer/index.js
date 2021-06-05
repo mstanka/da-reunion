d1c4792076d2c48f8f55ac93413fd2630af17e28
mstanka <mice78.mm@gmail.com>
2021-06-05 11:15:27 +0200

    add initial menu and footer

---
+++ components/Footer/index.js
@@ -0,0 +1,12 @@
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div> Czechitas, Digitální akademie: Web </div>
+      <div>© 2021 Markéta Staňková & Kateřina Levová</div>
+    </footer>
+  );
+};
+
+export default Footer;
