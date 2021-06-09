656749a6818f92e706c767beb7773c1ddee11e2e
Kateřina Levová <katerina.levova@gmail.com>
2021-06-09 17:10:31 +0200

    Gallery added to first two trips

---
+++ data/photosTrips/1/photos.js
@@ -0,0 +1,82 @@
+export const photos = [
+  {
+    src: '../../images/vylety/1/1.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '../../images/vylety/1/2.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '../../images/vylety/1/3.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '../../images/vylety/1/4.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '../../images/vylety/1/5.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '../../images/vylety/1/6.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '../../images/vylety/1/7.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '../../images/vylety/1/8.jpg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '../../images/vylety/1/9.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '../../images/vylety/1/10.jpg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '../../images/vylety/1/11.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '../../images/vylety/1/12.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '../../images/vylety/1/13.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '../../images/vylety/1/14.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '../../images/vylety/1/15.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '../../images/vylety/1/16.jpg',
+    width: 4,
+    height: 3,
+  },
+];
---
+++ data/photosTrips/2/photos.js
@@ -0,0 +1,57 @@
+export const photos = [
+  {
+    src: '../../images/vylety/2/1.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '../../images/vylety/2/2.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '../../images/vylety/2/3.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '../../images/vylety/2/4.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '../../images/vylety/2/5.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '../../images/vylety/2/6.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '../../images/vylety/2/7.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '../../images/vylety/2/8.jpg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '../../images/vylety/2/9.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '../../images/vylety/2/10.jpg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '../../images/vylety/2/11.jpg',
+    width: 4,
+    height: 3,
+  },
+];
