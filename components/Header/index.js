a7abdb91774c16aa584debafffbe2e1c96e587a4
mstanka <mice78.mm@gmail.com>
2021-06-09 17:50:56 +0200

    update intro section

--- components/Header/index.js
+++ components/Header/index.js
@@ -5,21 +5,20 @@ import Image from 'next/image';
 const Header = () => {
   return (
     <header className={styles.header}>
-      <h1>
-        <Link href="/">
-          <a className={styles.website_name}>
-            <div>Réunion </div> <div>našima očima</div>
-            <div className={styles.image}>
-              <Image
-                src={'/images/uvodni.jpg'}
-                width={1920}
-                height={900}
-                alt="Uvodní fotka"
-              />
-            </div>
-          </a>
-        </Link>
+      <h1 className={styles.website_name}>
+        <div>Réunion</div>
+        <div>našima očima</div>
       </h1>
+      <div className={styles.image}>
+        <Link href="/">
+          <Image
+            src={'/images/uvodni.jpg'}
+            width={1920}
+            height={900}
+            alt="Uvodní fotka"
+          />
+        </Link>
+      </div>
     </header>
   );
 };
