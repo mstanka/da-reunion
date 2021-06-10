1a3276541ac4f50af27d0e05e0515a754662acaa
Kateřina Levová <katerina.levova@gmail.com>
2021-06-10 09:40:53 +0200

    Conflict solved

--- components/AboutUs/index.js
+++ components/AboutUs/index.js
@@ -9,8 +9,11 @@ const AboutUs = () => {
         <h1> O nás </h1>
         <section className={styles.about_us}>
           <p>
-              Jsme holky, co ostrov prolezly skrz naskrz, nasbíraly poznatky každá svou cestou a během pobytů nasbíraly tipy od místních. Toto vše jsme jsme Vám sepsaly do našich stránek tak, aby se Vám informace co nejrychleji a nejkomfortněji hledaly.
-            </p>
+            Jsme holky, co ostrov prolezly skrz naskrz, nasbíraly poznatky každá
+            svou cestou a během pobytů nasbíraly tipy od místních. Toto vše jsme
+            jsme Vám sepsaly do našich stránek tak, aby se Vám informace co
+            nejrychleji a nejkomfortněji hledaly.
+          </p>
           <div className={styles.img_us}>
             <ImageMarketa
               src={'/images/marketa.jpg'}
@@ -23,9 +26,9 @@ const AboutUs = () => {
             <h2> Markéta </h2>
             <p>
               Turistika patří mezi moje oblíbené činnosti a pokud je kolem
-              krásná příroda, tak jako na ostrově Réunion, užívám si ji tím víc. Na
-              ostrově jsem strávila 14 dnů dovolené a prošla část vnitrozemí v
-              oblasti kráterů Mafate, Salazie a Cilaos. Během svých cest ráda
+              krásná příroda, tak jako na ostrově Réunion, užívám si ji tím víc.
+              Na ostrově jsem strávila 14 dnů dovolené a prošla část vnitrozemí
+              v oblasti kráterů Mafate, Salazie a Cilaos. Během svých cest ráda
               fotím a i touto obrazovou cestou vám ráda předám svoje zážitky a
               zkušenosti.
             </p>
@@ -42,7 +45,9 @@ const AboutUs = () => {
               dostanete zase od Markét. :) Ostrov má spoustu specifik, na které
               je třeba dávat si pozor, jako např. nevyzpytatelnost počasí a
               dopravy, ale také nejkrásnější západy slunce, panoramata hor a
-              modře čisté laguny a moře. Budu se těšit, že z našich zážitků získáte cenné info a na místě Vám pomůže!
+              modře čisté laguny a moře. Budu se těšit, že z našich zážitků
+              získáte cenné info a na místě Vám pomůže!
+            </p>
           </div>
           <div className={styles.img_us}>
             <ImageKata
