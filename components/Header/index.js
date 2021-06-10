36c9803aed5421a8b3cc3ea04e8b196685e42243
Kateřina Levová <katerina.levova@gmail.com>
2021-06-10 10:58:12 +0200

    Homepage content updated - second version

--- components/Header/index.js
+++ components/Header/index.js
@@ -4,22 +4,24 @@ import Image from 'next/image';
 
 const Header = () => {
   return (
-    <header className={styles.header}>
-      <h1 className={styles.website_name}>
-        <div>Réunion</div>
-        <div>našima očima</div>
-      </h1>
-      <div className={styles.image}>
-        <Link href="/">
-          <Image
-            src={'/images/uvodni.jpg'}
-            width={1920}
-            height={900}
-            alt="Uvodní fotka"
-          />
-        </Link>
-      </div>
-    </header>
+    <div className="link_to_homepage">
+      <Link href="/">
+        <header className={styles.header}>
+          <h1 className={styles.website_name}>
+            <div>Réunion</div>
+            <div>našima očima</div>
+          </h1>
+          <div className={styles.image}>
+            <Image
+              src={'/images/uvodni.jpg'}
+              width={1920}
+              height={900}
+              alt="Uvodní fotka"
+            />
+          </div>
+        </header>
+      </Link>
+    </div>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,91 @@
+import Link from 'next/link';
 import TipList from '../components/TipList';
 import TripList from '../components/TripList';
 import { getAllTips } from '../data/tips';
 import { getAllTrips } from '../data/trips';
+import ArrowRightIcon from '../components/icons/ArrowRightIcon';
 
 const Home = ({ tips, trips }) => {
   return (
     <>
       <main>
-        <div className="container">
+        <div className="container container_homepage">
           <div className="center">
             <h2>
-              Láká Vás ostrov Réunion? Nejste si jistí, zda je to pro Vás ?
+              Láká Vás ostrov Réunion? <br></br> Nebo si zatím nejste jisti, zda
+              je to pro Vás ?
             </h2>
             <h2>
               Mrkněte na naši{' '}
-              <a href="/galerie" className="link_to_page">
-                Galerii
-              </a>{' '}
+              <Link href="/galerie">
+                <span className="link_to_page">Galerii</span>
+              </Link>{' '}
               a uvidíte sami:{' '}
             </h2>
             <div className="Homepage_section">
               {/* <Gallery photos={photos} /> */}
-              <a href="/galerie">
-                <h2>šipka doprava na konci řádku</h2>
-              </a>
+              <Link href="/galerie">
+                <div className="know_more">
+                  <p>další fotky </p>
+                  <span className="arrow_know_more">
+                    {' '}
+                    <ArrowRightIcon />
+                  </span>
+                </div>
+              </Link>
             </div>
 
             <h2>
               Zajímá Vás, co čekat na ostrově? Mrkněte do našich{' '}
-              <a href="/tipy" className="link_to_page">
-                Tipů
-              </a>
+              <Link href="/tipy">
+                <span className="link_to_page">Tipů</span>
+              </Link>
               :
             </h2>
             <div className="Homepage_section">
               <TipList tips={tips} count={3} />
-              <a href="/tipy">
-                <h2>šipka doprava na konci řádku</h2>
-              </a>
+              <Link href="/tipy">
+                <div className="know_more">
+                  <p>další tipy</p>
+                  <span className="arrow_know_more">
+                    {' '}
+                    <ArrowRightIcon />
+                  </span>
+                </div>
+              </Link>
             </div>
             <h2>
               Jste rozhodnuti, že poletíte? Pojďme spolu naplánovat výlety !
             </h2>
             <h2>
-              Na naše Výlety se můžete buď proklikat přes{' '}
-              <a href="/mapa" className="link_to_page">
-                Mapu
-              </a>
+              Můžete se na ně proklikat buď přes{' '}
+              <Link href="/mapa">
+                <span className="link_to_page">Mapu </span>
+              </Link>
               :
             </h2>
             <h2>
               anebo si vyfiltrujte{' '}
-              <a href="/vylety" className="link_to_page">
-                Výlety
-              </a>{' '}
+              <Link href="/vylety">
+                <span className="link_to_page">Výlety</span>
+              </Link>{' '}
+            </h2>
+            <h3>
               přesně podle toho, <br></br> jestli Vás víc zajímá horolezení nebo
               koupání:
-            </h2>
+            </h3>
+
             <div className="Homepage_section">
               <TripList trips={trips} count={3} />
-              <a href="/vylety">šipka doprava na konci řádku</a>
+              <Link href="/vylety">
+                <div className="know_more">
+                  <p>další výlety</p>
+                  <span className="arrow_know_more">
+                    {' '}
+                    <ArrowRightIcon />
+                  </span>
+                </div>
+              </Link>
             </div>
           </div>
         </div>
