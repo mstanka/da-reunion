507cbf233aed043543f3a5a3026643d4792782e5
mstanka <mice78.mm@gmail.com>
2021-06-12 11:07:32 +0200

    update to solve conflict

--- pages/vylety/[id].js
+++ pages/vylety/[id].js
@@ -8,47 +8,45 @@ import LocationIcon from '../../components/icons/LocationIcon';
 
 const trips = getAllTrips();
 
-const Trip = ({ trip }) => {
-  return (
-    <>
-      {/* <Image
+{
+  /* <Image
         src={`/images/featuredImages/featured-${trip.id}.jpg`}
         width={1640}
         height={900}
         alt={trip.title}
-      /> */}
+      /> */
+}
 
-      <div className="content_trips">
-        <a href="/vylety">
-          <button className="btn_back btn_back_trips_1">
-            <p>Zpět na seznam výletů</p>{' '}
-          </button>
-        </a>
-        <a href="/mapa">
-          <button className="btn_back btn_back_trips_2">
-            <p>Zpět na mapu výletů</p>{' '}
-          </button>
-        </a>
-        <div className="short_info_trips">
-          <h1>{trip.title}</h1>
-          <p>{trip.about}</p>
-          <div className="icons_wrapper">
-            <DifficultyIcon />
-            <span>{trip.difficulty}</span>
-            <TypeIcon />
-            <span>{trip.type}</span>
-            <LocationIcon className="icon" />
-            <span>{trip.location}</span>
-          </div>
-        </div>
-        <p className="description">{trip.description}</p>
-        <GalleryImage
-          title={null}
-          id={trip.id}
-          numberOfImages={trip.numberOfImages}
-        />
+const Trip = ({ trip }) => {
+  return (
+    <div className="content_trips">
+      <div className="btns_wrapper">
+        <Link href="/vylety">
+          <a className="btn_back">Zpět na seznam výletů</a>
+        </Link>
+        <Link href="/mapa">
+          <a className="btn_back">Zpět na mapu výletů</a>
+        </Link>
       </div>
-    </>
+      <div className="short_info_trips">
+        <h1>{trip.title}</h1>
+        <p>{trip.about}</p>
+        <div className="icons_wrapper">
+          <DifficultyIcon />
+          <span>{trip.difficulty}</span>
+          <TypeIcon />
+          <span>{trip.type}</span>
+          <LocationIcon className="icon" />
+          <span>{trip.location}</span>
+        </div>
+      </div>
+      <p className="description">{trip.description}</p>
+      <GalleryImage
+        title={null}
+        id={trip.id}
+        numberOfImages={trip.numberOfImages}
+      />
+    </div>
   );
 };
 
