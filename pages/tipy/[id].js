072e4cf043dfc7f44cdc9c12722e07484bda140f
mstanka <mice78.mm@gmail.com>
2021-06-12 10:00:22 +0200

    correct typo

--- pages/tipy/[id].js
+++ pages/tipy/[id].js
@@ -6,17 +6,15 @@ const tips = getAllTips();
 
 const Tip = ({ tip }) => {
   return (
-    <>
-      <div className="content_tips">
-        <a href="/tipy">
-          <button className="btn_back btn_back_tips">
-            <p>Zpět na Obecné tipy</p>{' '}
-          </button>
-        </a>
-        <h1>{tip.title}</h1>
-        <p>{tip.description}</p>
-        <Image src={tip.image} width={1920} height={1280} />
-      </div>
+    <div className="content_tips">
+      <a href="/tipy">
+        <button className="btn_back btn_back_tips">
+          <p>Zpět na Obecné tipy</p>{' '}
+        </button>
+      </a>
+      <h1>{tip.title}</h1>
+      <p>{tip.description}</p>
+      <Image src={tip.image} width={1920} height={1280} />
     </div>
   );
 };
